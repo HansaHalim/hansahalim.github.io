602fe2d9d865d2b65a8f6b8fae39729109de76b9
Hansa Halim <hhalim@uwaterloo.ca>
2020-08-01 12:53:56 -0400

    Deploying New Pre-rendered React Website 2020

---
+++ precache-manifest.8405d121647b8caf081b9dcb1afedeb3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e00a5b0c81026616bcc54a78ca66c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c525d739658bba5440",
+    "url": "/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "bd7283d81919578a8f10",
+    "url": "/static/js/2.3fbe8cfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3fbe8cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c525d739658bba5440",
+    "url": "/static/js/main.dca35744.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "91e59eea70a434fc153b544a3835d057",
+    "url": "/static/media/FlappyShip.91e59eea.png"
+  },
+  {
+    "revision": "2b4429e654fb95263846c37ccab68554",
+    "url": "/static/media/background.2b4429e6.jpg"
+  },
+  {
+    "revision": "0214db210e400e50fe4d32b72fb9fee2",
+    "url": "/static/media/biquadris.0214db21.png"
+  },
+  {
+    "revision": "17778957dcf93c4242456753d880ec3c",
+    "url": "/static/media/down_icon.17778957.svg"
+  },
+  {
+    "revision": "1ec703f21d1c8ab986180ebcdf50ec2c",
+    "url": "/static/media/email.1ec703f2.png"
+  },
+  {
+    "revision": "5e80571e4db3a636431eccf35e3d1088",
+    "url": "/static/media/fluent.5e80571e.png"
+  },
+  {
+    "revision": "b9c82d4014d3d4e1af692070b807b48d",
+    "url": "/static/media/goosenet.b9c82d40.png"
+  },
+  {
+    "revision": "f55618d5939099f4d45fca68598f8711",
+    "url": "/static/media/hansa_halim.f55618d5.jpg"
+  },
+  {
+    "revision": "a445a74ae2050e514d67266213af30b8",
+    "url": "/static/media/openapp.a445a74a.png"
+  },
+  {
+    "revision": "e5054ac878eebf56b6f27e18e83aa581",
+    "url": "/static/media/realtimeq.e5054ac8.png"
+  },
+  {
+    "revision": "bbd597c3f5dab3f5ef8d04ceeebb546d",
+    "url": "/static/media/tauria.bbd597c3.png"
+  },
+  {
+    "revision": "127bef28e84efdd4e63beeae56c39d1f",
+    "url": "/static/media/tokopedia.127bef28.png"
+  }
+]);
